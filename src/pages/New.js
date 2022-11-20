439c4f688120da9fd5a2a1302319ed9840c6eed1
atk0426@naver.com <taekyung@DESKTOP-HRK1UQR.localdomain>
2022-11-20 18:28:17 +0900

    일기쓰기 오늘 날짜 표시

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,8 +1,38 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import MyButton from "../components/MyButton";
+import MyHeader from "../components/MyHeader";
+
+const getStringDate = (date) => {
+  return date.toISOString().slice(0, 10);
+};
+
 const New = () => {
+  console.log();
+  const [date, setDate] = useState(getStringDate(new Date()));
+
+  const navigate = useNavigate();
   return (
     <div>
-      <h1>New</h1>
-      <p>이곳은 일기 작성 페이지 입니다.</p>
+      <MyHeader
+        headText={"새 일기쓰기"}
+        leftChild={
+          <MyButton text={"< 뒤로가기"} onClick={() => navigate(-1)} />
+        }
+      />
+      <div>
+        <section>
+          <h4>오늘은 언제인가요?</h4>
+          <div className="input-box">
+            <input
+              className="input-date"
+              type="date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+            />
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
